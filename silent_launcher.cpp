bcaacbed7004c48d7f7d17ab4586de70948539f5
sanyabeast <a.gvrnsk@gmail.com>
2020-07-31 10:56:38 +0300

    changed working principles

--- silent_launcher.cpp
+++ silent_launcher.cpp
@@ -8,37 +8,49 @@
 #include "silent_launcher.h"
 
 std::string get_app_name();
-
-#define LOG(text) OutputDebugString(_T( "SILENT LAUNCER X: " text "\n"));
-#define CONCAT (x, y) x y
+std::string get_executable_name();
 
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
-    std::string app_name = get_app_name();
-    LOG("init instance ...");
-    std::string extension = ".bat";
-    std::string script_name = app_name + extension;
-    WinExec(script_name.c_str(), 0);
-    return TRUE;
-
-    return 0;
+std::string replace(std::string str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); 
+    }
+    return str;
 }
 
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+    std::string command_code = get_executable_name();
+    std::string launch_type = command_code;
+    command_code = command_code.substr(1, command_code.length() - 5);
+    launch_type = launch_type.substr(0, 1); 
+    
+    command_code = replace( command_code, "[fsh]", "/" );
+    command_code = replace( command_code, "[bsh]", "\\");
+    command_code = replace( command_code, "[qot]", "\"");
+    command_code = replace( command_code, "[arx]", "*");
+    command_code = replace( command_code, "[lth]", "<");
+    command_code = replace( command_code, "[gth]", ">");
+    command_code = replace( command_code, "[cln]", ":");
+    command_code = replace( command_code, "[bar]", "|");
+    command_code = replace( command_code, "[qst]", "?");
+    WinExec(command_code.c_str(), launch_type.compare("~") == 0 ? 0 : 1 );
+    return TRUE;
+}
 
-std::string get_app_name() {
+std::string get_executable_name() {
     char myexepath[MAX_PATH] = { 0 };
     DWORD returnCode = GetModuleFileNameA(NULL, myexepath, MAX_PATH);
-    if (returnCode != 0 && returnCode < MAX_PATH)
-    {
+    if (returnCode != 0 && returnCode < MAX_PATH) {
         std::string filepath(myexepath);
         filepath = filepath.substr(filepath.find_last_of('\\') + 1);
         std::ostringstream ss;
         ss << filepath;
         std::string name = ss.str();
-        return name.substr(0, name.length() - 4);
+        return name;
     }
-    else
-    {
-        return "";
+    else {
+        return "~exit.exe";
     }
 }
